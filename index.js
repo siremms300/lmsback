f06611a3f1f241f736481e739698df730eff6534
Idoga Emmanuel <siremms300@gmail.com>
2024-12-31 05:09:02 -0800

    changed the cors

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin:"https://lms-teal-five.vercel.app/",
+    origin:"http://localhost:5173",
     credentials:true
 }));
  
