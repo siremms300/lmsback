23537d566fe1c70e0f46e9a9724230ae19ab4891
Idoga Emmanuel <siremms300@gmail.com>
2024-12-31 05:43:08 -0800

    changed the cors

--- controllers/coursePurchase.controller.js
+++ controllers/coursePurchase.controller.js
@@ -39,8 +39,8 @@ export const createCheckoutSession = async (req, res) => {
         },
       ],
       mode: "payment",
-      success_url: `http://localhost:5173/course-progress/${courseId}`, // once payment successful redirect to course progress page
-      cancel_url: `http://localhost:5173/course-detail/${courseId}`,
+      success_url: `https://lmsfront.onrender.com3/course-progress/${courseId}`, // once payment successful redirect to course progress page
+      cancel_url: `https://lmsfront.onrender.com3/course-detail/${courseId}`,
       metadata: {
         courseId: courseId,
         userId: userId,
